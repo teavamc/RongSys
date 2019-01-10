627e237430b70e3a5e96ee0a843773c1e47e511b
teavamc <teavamc@163.com>
2019-01-10 18:01:08 +0800

    新增应急广播base控制器

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/BroadBaseController.java
@@ -0,0 +1,17 @@
+package com.ruoyi.web.controller.broad;
+
+import com.ruoyi.common.config.Global;
+import com.ruoyi.framework.web.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BroadBaseController extends BaseController {
+
+    @GetMapping("/broad/todo")
+    public String todo()
+    {
+        return "borad/todo";
+    }
+}
