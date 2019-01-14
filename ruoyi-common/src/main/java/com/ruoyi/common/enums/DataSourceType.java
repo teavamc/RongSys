8428d1df3709e8d2d59ac51d765787cb65118212
teavamc <teavamc@163.com>
2019-01-14 17:05:32 +0800

    新增五个数据源，实现多源数据操作。 禁用SALVE源，以后做主从分离使用

--- ruoyi-common/src/main/java/com/ruoyi/common/enums/DataSourceType.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/enums/DataSourceType.java
@@ -17,4 +17,29 @@ public enum DataSourceType
      */
     SLAVE,
 
+    /**
+     * 思信广播
+     */
+    SXBROAD,
+
+    /**
+     * 思信广播用户
+     */
+    SXUSER,
+
+    /**
+     * 思信山洪预警
+     */
+    SXRIVERVIS,
+
+    /**
+     * 思信信息发布平台
+     */
+    SXINFOM,
+
+    /**
+     * 思信村务系统
+     */
+    SXVILLAGE
+
 }
--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/DruidConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/DruidConfig.java
@@ -35,13 +35,59 @@ public class DruidConfig
         return DruidDataSourceBuilder.create().build();
     }
 
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.sxbroad")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.sxbroad", name = "enabled", havingValue = "true")
+    public DataSource sxbroadDataSource()
+    {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.sxuser")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.sxuser", name = "enabled", havingValue = "true")
+    public DataSource sxuserDataSource()
+    {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.sxinfom")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.sxinfom", name = "enabled", havingValue = "true")
+    public DataSource sxinfomDataSource()
+    {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.sxrivervis")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.sxrivervis", name = "enabled", havingValue = "true")
+    public DataSource sxrivervisDataSource()
+    {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.sxvillage")
+    @ConditionalOnProperty(prefix = "spring.datasource.druid.sxvillage", name = "enabled", havingValue = "true")
+    public DataSource sxvillageDataSource()
+    {
+        return DruidDataSourceBuilder.create().build();
+    }
+
     @Bean(name = "dynamicDataSource")
     @Primary
-    public DynamicDataSource dataSource(DataSource masterDataSource, DataSource slaveDataSource)
-    {
+    public DynamicDataSource dataSource(
+            DataSource masterDataSource, DataSource slaveDataSource, DataSource sxbroadDataSource, DataSource sxuserDataSource,
+            DataSource sxrivervisDataSource, DataSource sxinfomDataSource, DataSource sxvillageDataSource){
         Map<Object, Object> targetDataSources = new HashMap<>();
         targetDataSources.put(DataSourceType.MASTER.name(), masterDataSource);
         targetDataSources.put(DataSourceType.SLAVE.name(), slaveDataSource);
+        targetDataSources.put(DataSourceType.SXBROAD.name(), sxbroadDataSource);
+        targetDataSources.put(DataSourceType.SXUSER.name(), sxuserDataSource);
+        targetDataSources.put(DataSourceType.SXRIVERVIS.name(), sxrivervisDataSource);
+        targetDataSources.put(DataSourceType.SXINFOM.name(), sxinfomDataSource);
+        targetDataSources.put(DataSourceType.SXVILLAGE.name(), sxvillageDataSource);
         return new DynamicDataSource(masterDataSource, targetDataSources);
     }
 }
