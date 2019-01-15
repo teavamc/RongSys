cd6e3d7c9d11a7d8e211ddd4605f7662707180f6
teavamc <teavamc@163.com>
2019-01-15 13:25:26 +0800

    新增应急广播设备信息终端维修记录模块

---
+++ sx-broad/src/main/java/com/ruoyi/broad/service/impl/MaintainServiceImpl.java
@@ -0,0 +1,91 @@
+package com.ruoyi.broad.service.impl;
+
+import java.util.List;
+
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.broad.mapper.MaintainMapper;
+import com.ruoyi.broad.domain.Maintain;
+import com.ruoyi.broad.service.IMaintainService;
+import com.ruoyi.common.support.Convert;
+
+/**
+ * 终端维护记录 服务层实现
+ * 
+ * @author 张超
+ * @date 2019-01-15
+ */
+@Service
+public class MaintainServiceImpl implements IMaintainService 
+{
+	@Autowired
+	private MaintainMapper maintainMapper;
+
+	/**
+     * 查询终端维护记录信息
+     * 
+     * @param tmid 终端维护记录ID
+     * @return 终端维护记录信息
+     */
+    @Override
+	@DataSource(value = DataSourceType.SLAVE)
+	public Maintain selectMaintainById(Integer tmid)
+	{
+	    return maintainMapper.selectMaintainById(tmid);
+	}
+	
+	/**
+     * 查询终端维护记录列表
+     * 
+     * @param maintain 终端维护记录信息
+     * @return 终端维护记录集合
+     */
+	@Override
+	@DataSource(value = DataSourceType.SLAVE)
+	public List<Maintain> selectMaintainList(Maintain maintain)
+	{
+	    return maintainMapper.selectMaintainList(maintain);
+	}
+	
+    /**
+     * 新增终端维护记录
+     * 
+     * @param maintain 终端维护记录信息
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SLAVE)
+	public int insertMaintain(Maintain maintain)
+	{
+	    return maintainMapper.insertMaintain(maintain);
+	}
+	
+	/**
+     * 修改终端维护记录
+     * 
+     * @param maintain 终端维护记录信息
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SLAVE)
+	public int updateMaintain(Maintain maintain)
+	{
+	    return maintainMapper.updateMaintain(maintain);
+	}
+
+	/**
+     * 删除终端维护记录对象
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SLAVE)
+	public int deleteMaintainByIds(String ids)
+	{
+		return maintainMapper.deleteMaintainByIds(Convert.toStrArray(ids));
+	}
+	
+}
