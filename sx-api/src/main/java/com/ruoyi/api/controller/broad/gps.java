31c34beeb9e6e4664ef9e97f235ef4568ac0cb56
teavamc <teavamc@163.com>
2019-01-16 21:50:36 +0800

    新增应急广播终端信息分组

--- sx-api/src/main/java/com/ruoyi/api/controller/broad/gps.java
+++ sx-api/src/main/java/com/ruoyi/api/controller/broad/gps.java
@@ -3,12 +3,9 @@ package com.ruoyi.api.controller.broad;
 import com.ruoyi.broad.domain.Conditions;
 import com.ruoyi.broad.service.IConditionsService;
 import com.ruoyi.framework.web.base.BaseController;
-import com.ruoyi.framework.web.domain.server.Sys;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,9 +20,7 @@ public class gps extends BaseController
     @GetMapping()
     public List<Conditions> searchrandom()
     {
-        System.out.println("我被调用了啊！！！！！！！！！！！！！！");
         List<Conditions> search = conditionsService.selectConditionsByRandom();
-        System.out.println("对象实例了啊！！！！！！！！！！");
         return search;
     }
 
