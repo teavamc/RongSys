dfb059365dc0c5f7e699104dd82f2584c76b7c80
teavamc <teavamc@163.com>
2019-01-17 14:08:39 +0800

    修正了Merge后丢失的Api模块依赖配置

--- sx-api/src/main/java/com/ruoyi/api/controller/broad/gps.java
+++ sx-api/src/main/java/com/ruoyi/api/controller/broad/gps.java
@@ -2,7 +2,9 @@ package com.ruoyi.api.controller.broad;
 
 import com.ruoyi.broad.domain.Conditions;
 import com.ruoyi.broad.service.IConditionsService;
+
 import com.ruoyi.framework.web.base.BaseController;
+import com.ruoyi.framework.web.domain.server.Sys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,9 @@ public class gps extends BaseController
     @GetMapping()
     public List<Conditions> searchrandom()
     {
+        System.out.println("如果执行了这里显示就会看到这句话");
         List<Conditions> search = conditionsService.selectConditionsByRandom();
+        System.out.println("如果执行完成了这里显示就会看到这句话");
         return search;
     }
 
