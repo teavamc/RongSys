479bc1a8e168c2abdf407aed5256a34fa6e072bc
ZhangHongQuan <975669814@qq.com>
2019-01-19 17:01:23 +0800

    1.新增了村务管理中微信用户管理的业务功能及相应的页面

---
+++ sx-village/src/main/java/com/ruoyi/village/service/impl/WuserServiceImpl.java
@@ -0,0 +1,91 @@
+package com.ruoyi.village.service.impl;
+
+import java.util.List;
+
+import com.ruoyi.common.annotation.DataSource;
+import com.ruoyi.common.enums.DataSourceType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.village.mapper.WuserMapper;
+import com.ruoyi.village.domain.Wuser;
+import com.ruoyi.village.service.IWuserService;
+import com.ruoyi.common.support.Convert;
+
+/**
+ * 用户基本 服务层实现
+ * 
+ * @author 张鸿权
+ * @date 2019-01-19
+ */
+@Service
+public class WuserServiceImpl implements IWuserService 
+{
+	@Autowired
+	private WuserMapper wuserMapper;
+
+	/**
+     * 查询用户基本信息
+     * 
+     * @param uid 用户基本ID
+     * @return 用户基本信息
+     */
+    @Override
+	@DataSource(value = DataSourceType.SXVILLAGE)
+	public Wuser selectWuserById(Integer uid)
+	{
+	    return wuserMapper.selectWuserById(uid);
+	}
+	
+	/**
+     * 查询用户基本列表
+     * 
+     * @param wuser 用户基本信息
+     * @return 用户基本集合
+     */
+	@Override
+	@DataSource(value = DataSourceType.SXVILLAGE)
+	public List<Wuser> selectWuserList(Wuser wuser)
+	{
+	    return wuserMapper.selectWuserList(wuser);
+	}
+	
+    /**
+     * 新增用户基本
+     * 
+     * @param wuser 用户基本信息
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SXVILLAGE)
+	public int insertWuser(Wuser wuser)
+	{
+	    return wuserMapper.insertWuser(wuser);
+	}
+	
+	/**
+     * 修改用户基本
+     * 
+     * @param wuser 用户基本信息
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SXVILLAGE)
+	public int updateWuser(Wuser wuser)
+	{
+	    return wuserMapper.updateWuser(wuser);
+	}
+
+	/**
+     * 删除用户基本对象
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	@Override
+	@DataSource(value = DataSourceType.SXVILLAGE)
+	public int deleteWuserByIds(String ids)
+	{
+		return wuserMapper.deleteWuserByIds(Convert.toStrArray(ids));
+	}
+	
+}
