ae0a4df5a0861b447ae937457c20b194a17c0049
ZhangHongQuan <975669814@qq.com>
2019-01-20 11:53:13 +0800

    1.新增了村务管理中党员基本信息管理的业务功能及相应的页面； 2.新增了村务管理中党务值班的业务功能及相应的页面； 3.新增了村务管理中党务自评互评的业务功能及相应的页面；

---
+++ sx-village/src/main/java/com/ruoyi/village/service/IPartyworkService.java
@@ -0,0 +1,54 @@
+package com.ruoyi.village.service;
+
+import com.ruoyi.village.domain.Partywork;
+import java.util.List;
+
+/**
+ * 党员值班 服务层
+ * 
+ * @author 张鸿权
+ * @date 2019-01-20
+ */
+public interface IPartyworkService 
+{
+	/**
+     * 查询党员值班信息
+     * 
+     * @param id 党员值班ID
+     * @return 党员值班信息
+     */
+	public Partywork selectPartyworkById(Integer id);
+	
+	/**
+     * 查询党员值班列表
+     * 
+     * @param partywork 党员值班信息
+     * @return 党员值班集合
+     */
+	public List<Partywork> selectPartyworkList(Partywork partywork);
+	
+	/**
+     * 新增党员值班
+     * 
+     * @param partywork 党员值班信息
+     * @return 结果
+     */
+	public int insertPartywork(Partywork partywork);
+	
+	/**
+     * 修改党员值班
+     * 
+     * @param partywork 党员值班信息
+     * @return 结果
+     */
+	public int updatePartywork(Partywork partywork);
+		
+	/**
+     * 删除党员值班信息
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	public int deletePartyworkByIds(String ids);
+	
+}
