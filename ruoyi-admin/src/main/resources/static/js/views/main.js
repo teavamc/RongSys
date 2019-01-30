c881d09325b07e6c7bd78df4fe2e8d642b5a702d
teavamc <teavamc@163.com>
2019-01-30 23:52:15 +0800

    1、完善主页控制台九个工具模块 2、JS程序更加规范 3、调整了若干bug

--- ruoyi-admin/src/main/resources/static/js/views/main.js
+++ ruoyi-admin/src/main/resources/static/js/views/main.js
@@ -1,17 +1,9 @@
 // 等待页面DOM加载完后自动加载
 function onload() {
     // 广播加载
-    $.ajax({
-        type: "GET",
-        url: "/api/bcount/bindex",
-        dataType: "json",
-        success: function(data){
-            $("#get_time").html(data.time);
-            $("#count_dev").html(data.data.dev);
-            $("#count_run").html(data.data.run);
-            $("#count_stop").html(data.data.stop);
-        }
-    });
+    flush_broad();
+    //村务加载
+    flush_village();
 }
 
 function flush_broad() {
@@ -20,12 +12,28 @@ function flush_broad() {
         url: "/api/bcount/bindex",
         dataType: "json",
         success: function(data){
-
-            $("#get_time").html(data.time);
+            $("#fls_cb_time").html(data.time);
             $("#count_dev").html(data.data.dev);
             $("#count_run").html(data.data.run);
             $("#count_stop").html(data.data.stop);
-
         }
     });
 };
+
+function flush_village() {
+    $.ajax({
+        type:"GET",
+        url:"/api/count/p_m",
+        dataType:"json",
+        success:function (data) {
+            $("#fls_cv_time").html(data.time);
+            $("#count_ms").html(data.data[0].mwoman);
+            $("#count_mm").html(data.data[0].msum);
+            $("#count_mw").html(data.data[0].pwoman);
+            $("#count_ps").html(data.data[0].mman);
+            $("#count_p_m").html(data.data[0].pman);
+            $("#count_pw").html(data.data[0].psum);
+        }
+    })
+
+}
