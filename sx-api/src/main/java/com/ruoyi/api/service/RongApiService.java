df3df795c75f7c33d510bd1e38738f6b0e097757
teavamc <teavamc@163.com>
2019-02-04 23:45:02 +0800

    List<Map<Object,String>>业务层快排思路

--- sx-api/src/main/java/com/ruoyi/api/service/RongApiService.java
+++ sx-api/src/main/java/com/ruoyi/api/service/RongApiService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  *  接口业务处理封装
@@ -35,6 +36,13 @@ public class RongApiService {
         return res;
     }
 
+    /**
+        * 将其他业务中的HashMap(JavaBean)进行统一封装成API-JSON数据
+        * @author 张超 teavamc
+        * @date 2019/2/4
+        * @param [obj]
+        * @return com.ruoyi.api.domain.RongApiRes
+        */
     public static RongApiRes get_bean(Object obj){
         RongApiRes res = new RongApiRes();
         if(obj!=null){
@@ -46,16 +54,16 @@ public class RongApiService {
         return res;
     }
 
-//    public static RongApiRes get_hashmap(Object obj){
-//        RongApiRes res = new RongApiRes();
-//
-//        if(obj!=null){
-//            res = res.success("已查询到1条数据");
-//            res.put("data",obj);
-//        }else {
-//            res = res.error("数据获取失败");
-//        }
-//        return res;
-//    }
+    public static List<HashMap<String,Object>> list_quickSort(List<HashMap<String,Object>> list){
+        List<HashMap<String,Object>> pre = list;
+        //1.取list[0]的数据，找到value最大的key
+        Object[] keys = ((list.get(0)).keySet()).toArray();
+        //2.遍历这个数组 拿到值最大的key
+        //3.list就按照这个Key的Value进行快排
+
+
+        return list;
+    }
+
 
 }
