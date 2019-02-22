f23bdc05634342ed58492dfb4ef8bb84779b5a3e
teavamc <teavamc@163.com>
2019-02-22 13:07:27 +0800

    1、修改 host 为 120.79.42.11 自己的服务器 2、更改 流媒体地址为 rtmp://120.79.42.11:1936/live 3、准备新环境部署

--- ruoyi-admin/src/main/resources/static/js/views/live/livestreamtest.js
+++ ruoyi-admin/src/main/resources/static/js/views/live/livestreamtest.js
@@ -54,7 +54,7 @@ function connectWS() {
     // 当前主机
     var hostaddress = window.location.host + path.substring(0,path.substr(1).indexOf('/')+1);
     // 后台控制器url
-    // 控制推流器地址
+    // 控制推流器地址 ws信息地址
     var target = "/live_socket";
     // 将http协议换成ws
     if (window.location.protocol == 'http:') {
