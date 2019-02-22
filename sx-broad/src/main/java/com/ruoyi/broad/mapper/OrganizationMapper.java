409365c90fa7c696d15b861c9e2ac0df6e41bd97
teavamc <teavamc@163.com>
2019-02-22 12:53:57 +0800

    1、新增根据设备tid/IMEI号查询Organization模型的业务方法

--- sx-broad/src/main/java/com/ruoyi/broad/mapper/OrganizationMapper.java
+++ sx-broad/src/main/java/com/ruoyi/broad/mapper/OrganizationMapper.java
@@ -14,10 +14,18 @@ public interface OrganizationMapper
 	/**
 	 * 查询终端地域信息
 	 *
-	 * @param tid 终端地域ID
+	 * @param aid 终端地域ID
 	 * @return 终端地域信息
 	 */
-	public Organization selectOrganizationById(String tid);
+	public Organization selectOrganizationById(String aid);
+
+	/**
+	 * 查询终端地域信息
+	 *
+	 * @param tid 设备IMEI
+	 * @return 终端地域信息
+	 */
+	public Organization selectOrganizationByTid(String tid);
 
 	/**
 	 * 查询终端地域列表
