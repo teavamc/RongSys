5aa74e115df88bc5e5cdd57b47712cf68dc8d3ca
Abelsjcheng <429433693@qq.com>
2019-03-31 19:22:38 +0800

    新增流量限制数据层接口

--- sx-broad/src/main/java/com/ruoyi/broad/mapper/TrafficMapper.java
+++ sx-broad/src/main/java/com/ruoyi/broad/mapper/TrafficMapper.java
@@ -1,62 +1,69 @@
 package com.ruoyi.broad.mapper;
 
 import com.ruoyi.broad.domain.Traffic;
-import java.util.List;	
+import java.util.List;
 
 /**
  * 流量 数据层
- * 
+ *
  * @author 张超
  * @date 2019-01-15
  */
-public interface TrafficMapper 
+public interface TrafficMapper
 {
 	/**
      * 查询流量信息
-     * 
+     *
      * @param ttid 流量ID
      * @return 流量信息
      */
 	public Traffic selectTrafficById(Integer ttid);
-	
+
 	/**
      * 查询流量列表
-     * 
+     *
      * @param traffic 流量信息
      * @return 流量集合
      */
 	public List<Traffic> selectTrafficList(Traffic traffic);
-	
+
 	/**
      * 新增流量
-     * 
+     *
      * @param traffic 流量信息
      * @return 结果
      */
 	public int insertTraffic(Traffic traffic);
-	
+
 	/**
      * 修改流量
-     * 
+     *
      * @param traffic 流量信息
      * @return 结果
      */
 	public int updateTraffic(Traffic traffic);
-	
+
+	/**
+	 * 修改流量限制
+	 *
+	 * @param traffic 流量信息
+	 * @return 结果
+	 */
+	public int updateTrafficSet(Traffic traffic);
 	/**
      * 删除流量
-     * 
+     *
      * @param ttid 流量ID
      * @return 结果
      */
 	public int deleteTrafficById(Integer ttid);
-	
+
 	/**
      * 批量删除流量
-     * 
+     *
      * @param ttids 需要删除的数据ID
      * @return 结果
      */
 	public int deleteTrafficByIds(String[] ttids);
-	
+
 }
