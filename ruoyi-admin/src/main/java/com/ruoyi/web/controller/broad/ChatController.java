ef27505ec9dfdf6cf470c20268ea9a2b7c1097c2
Boba-Syu <1914808114@qq.com>
2019-04-28 12:03:34 +0800

    聊天界面初版

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/ChatController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/ChatController.java
@@ -1,10 +1,14 @@
 package com.ruoyi.web.controller.broad;
 
 
+import com.ruoyi.framework.util.ShiroUtils;
+import com.ruoyi.system.domain.SysUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.ruoyi.framework.web.base.BaseController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 聊天室
@@ -18,7 +22,9 @@ public class ChatController extends BaseController {
     private String prefix = "broad/chat";
 
     @GetMapping()
-    public String chat() {
+    public String chat(HttpSession session) {
+        SysUser user = ShiroUtils.getSysUser(); //从session中获取当前登陆用户的userid
+        session.setAttribute("userName", user.getUserName());
         return prefix + "/chat";
     }
 }
