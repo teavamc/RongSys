6433506533ab1068d702fbe19e1a6dc0b5fcb862
ZhangHongQuan <975669814@qq.com>
2019-06-18 11:11:40 +0800

    设备信息---终端坐标功能修改：支持更多的信息同时显示，增加查询选项功能，便于日常查询;

--- sx-broad/src/main/java/com/ruoyi/broad/mapper/ManagementMapper.java
+++ sx-broad/src/main/java/com/ruoyi/broad/mapper/ManagementMapper.java
@@ -9,58 +9,58 @@ import java.util.List;
 
 /**
  * 终端管理 数据层
- * 
+ *
  * @author 张超
  * @date 2019-01-15
  */
-public interface ManagementMapper 
+public interface ManagementMapper
 {
 	/**
-     * 查询终端管理信息
-     * 
-     * @param tid 终端管理ID
-     * @return 终端管理信息
-     */
+	 * 查询终端管理信息
+	 *
+	 * @param tid 终端管理ID
+	 * @return 终端管理信息
+	 */
 	public Management selectManagementById(String tid);
-	
+
 	/**
-     * 查询终端管理列表
-     * 
-     * @param aid 终端管理信息
-     * @return 终端管理集合
-     */
-	public List selectManagementList(String aid);
-	
+	 * 查询终端管理列表
+	 *
+	 * @param management 终端管理信息
+	 * @return 终端管理集合
+	 */
+	public List selectManagementList(Management management);
+
 	/**
-     * 新增终端管理
-     * 
-     * @param management 终端管理信息
-     * @return 结果
-     */
+	 * 新增终端管理
+	 *
+	 * @param management 终端管理信息
+	 * @return 结果
+	 */
 	public int insertManagement(Management management);
-	
+
 	/**
-     * 修改终端管理
-     * 
-     * @param management 终端管理信息
-     * @return 结果
-     */
+	 * 修改终端管理
+	 *
+	 * @param management 终端管理信息
+	 * @return 结果
+	 */
 	public int updateManagement(Management management);
-	
+
 	/**
-     * 删除终端管理
-     * 
-     * @param tid 终端管理ID
-     * @return 结果
-     */
+	 * 删除终端管理
+	 *
+	 * @param tid 终端管理ID
+	 * @return 结果
+	 */
 	public int deleteManagementById(String tid);
-	
+
 	/**
-     * 批量删除终端管理
-     * 
-     * @param tids 需要删除的数据ID
-     * @return 结果
-     */
+	 * 批量删除终端管理
+	 *
+	 * @param tids 需要删除的数据ID
+	 * @return 结果
+	 */
 	public int deleteManagementByIds(String[] tids);
 
 	/**
@@ -80,29 +80,29 @@ public interface ManagementMapper
 	public List<Managementgps> selectManagementAll();
 
 	/**
-	    * 按照终端地址进行运行状态的分组统计
-	    * @author 张超 teavamc
-	    * @date 2019/1/25
-	    * @param []
-	    * @return java.util.List<com.ruoyi.broad.domain.Tersga>
-	    */
+	 * 按照终端地址进行运行状态的分组统计
+	 * @author 张超 teavamc
+	 * @date 2019/1/25
+	 * @param []
+	 * @return java.util.List<com.ruoyi.broad.domain.Tersga>
+	 */
 	public List<Tersga> selecttersga();
 
 	/**
-	    * 回传目前设备挂载总数,运行数，停止数
-	    * @author 张超 teavamc
-	    * @date 2019/1/25
-	    * @param []
-	    * @return com.ruoyi.broad.domain.Tersga
-	    */
+	 * 回传目前设备挂载总数,运行数，停止数
+	 * @author 张超 teavamc
+	 * @date 2019/1/25
+	 * @param []
+	 * @return com.ruoyi.broad.domain.Tersga
+	 */
 	public Tersga sumters();
 
 	/**
-	    * 按照终端地址进行运行状态的分组统计,包括分组地区、挂在数量、运行数量、停止数量
-	    * @author 张超 teavamc
-	    * @date 2019/1/25
-	    * @param []
-	    * @return java.util.List<com.ruoyi.broad.domain.Tersga>
-	    */
+	 * 按照终端地址进行运行状态的分组统计,包括分组地区、挂在数量、运行数量、停止数量
+	 * @author 张超 teavamc
+	 * @date 2019/1/25
+	 * @param []
+	 * @return java.util.List<com.ruoyi.broad.domain.Tersga>
+	 */
 	public List<Tersga> sumterm();
 }
